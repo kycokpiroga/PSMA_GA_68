e2941b0928b51a8101072043dc9a5dac284f7ba7
kycokpiroga <38364329+kycokpiroga@users.noreply.github.com>
2024-08-17 21:20:05 +0500

    multipage-1
    multipage

---
+++ main_page.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from pages.page_1 import func_page_1
+def main():
+    st.sidebar.subheader('Page selection')
+    page_selection = st.sidebar.selectbox('Please select a page',['Welcome',
+    'Second'])
+    pages_main = {
+    'Welcome': main_page,
+    'Second': page_1}
+    # Run selected page
+    pages_main[page_selection]()
+def main_page():
+        st.set_page_config(
+        page_title="Welcome",
+        page_icon="👋",)
+        st.header('DOTA-PSMA-617_Ga-68', divider='rainbow')
+        #st.image('phone.png')
+        st.markdown("#### Моделирование активности препарта в МБк")
+        st.divider()
+        st.markdown ("- ### Цели и ожидания пользователей")
+        st.markdown ("- ### Возможные риски и ограничения модели")  
+        st.divider()
+        st.button("Reset", type="primary")
+def run_page_1():
+        func_page_1()
+if __name__ == "__main__":   
+    main()
--- pages/page_1.py
+++ pages/page_1.py
@@ -2,16 +2,16 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import wget
-def run():
+def func_page_1():
     st.set_page_config(
     page_title="Second",
     page_icon="👋",)
     st.header('DOTA-PSMA-617_Ga-68_2', divider='rainbow')
 progress_text = st.empty()
 progress_bar = st.progress(0)
-def streamlit_progress_bar(current,total,width):
+"""def streamlit_progress_bar(current,total,width):
     percent = int((current/total)*100)
     progress_text.subheader('Progress: {}%'.format(percent))
     progress_bar.progress(percent)
     wget.download('file URL',
-    bar=streamlit_progress_bar)
+    bar=streamlit_progress_bar)"""
