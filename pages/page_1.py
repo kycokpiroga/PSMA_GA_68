8318ab4de2d337245512d315a9c11061280d7692
kycokpiroga <38364329+kycokpiroga@users.noreply.github.com>
2024-08-18 22:02:11 +0500

    Update page_1.py
    update

--- pages/page_1.py
+++ pages/page_1.py
@@ -8,6 +8,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.pipeline import make_pipeline
 from sklearn.metrics import mean_squared_error, r2_score
 import datetime
+
 def func_page_1():
     st.set_page_config(
         page_title="PSMA",
@@ -37,6 +38,50 @@ def ml_polynomial(features, output):
     
     return self_pred, mse, r2
 
+def user_u():
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        # Ввод даты
+        date_0 = st.date_input("Выберите дату из паспорта генератора", datetime.date.today())
+
+    with col2:
+        # Ввод даты
+        date_1 = st.date_input("Выберите дату и время передачи на КК последнего синтеза или ТЭ", datetime.date.today())
+
+        # Ввод времени в формате строки
+        time_str = st.text_input("Введите время", "07:30")
+
+        # Преобразование строки в объект времени
+        try:
+            time_1 = datetime.datetime.strptime(time_str, "%H:%M").time()
+        except ValueError:
+            st.error("Неверный формат времени. Пожалуйста, используйте формат HH:MM.")
+        # Объединение даты и времени
+        datetime_selected_1 = datetime.datetime.combine(date_1, time_1)
+
+    with col3:
+        # Ввод даты
+        date_2 = st.date_input("Выберите дату и время передачи на КК предполагаемого синтеза", datetime.date.today())
+
+        # Ввод времени в формате строки
+        time_str_2 = st.text_input("Введите время", "11:30")
+
+        # Преобразование строки в объект времени
+        try:
+            time_2 = datetime.datetime.strptime(time_str_2, "%H:%M").time()
+        except ValueError:
+            st.error("Неверный формат времени. Пожалуйста, используйте формат HH:MM.")
+
+        # Объединение даты и времени
+        datetime_selected_2 = datetime.datetime.combine(date_2, time_2)
+
+    day_from_calib_gen = (date_2 - date_0).days
+    st.write("Количество дней между датой калибровки и предполгаемым синтезом:", day_from_calib_gen)
+
+    k_rec = (datetime_selected_2 - datetime_selected_1).total_seconds() / 60
+    st.write("Количество минут между синтезами:", k_rec)
+    st.write("Количество часов между синтезами:",round(k_rec / 60, 2))
+
 # Prepare the data
 features, output = preparation()
 
@@ -46,40 +91,6 @@ predictions, mse, r2 = ml_polynomial(features, output)
 st.write("Mean Squared Error:", mse)
 st.write("R-squared Score:", r2)
 st.header('Внесите данные, Дата калибровки генератора галлия, дата и время предыдущего синтеза или ТЭ, дата и время предполагаемого синтеза', divider='rainbow')
-col1, col2, col3 = st.columns(3)
-with col1:
-    # Ввод даты
-    date = st.date_input("Выберите дату из паспорта генератора", datetime.date.today())
 
-with col2:
-     # Ввод даты
-    date = st.date_input("Выберите дату и время передачи на КК последнего синтеза или ТЭ", datetime.date.today())
-
-    # Ввод времени в формате строки
-    time_str = st.text_input("Введите время", "12:00")
-
-    # Преобразование строки в объект времени
-    try:
-        time = datetime.datetime.strptime(time_str, "%H:%M").time()
-    except ValueError:
-        st.error("Неверный формат времени. Пожалуйста, используйте формат HH:MM.")
-    # Объединение даты и времени
-    datetime_selected = datetime.datetime.combine(date, time)
-
-with col3:
-     # Ввод даты
-    date_2 = st.date_input("Выберите дату и время передачи на КК предполагаемого синтеза", datetime.date.today())
-
-    # Ввод времени в формате строки
-    time_str_2 = st.text_input("Введите время", "13:00")
-
-    # Преобразование строки в объект времени
-    try:
-        time_2 = datetime.datetime.strptime(time_str_2, "%H:%M").time()
-    except ValueError:
-        st.error("Неверный формат времени. Пожалуйста, используйте формат HH:MM.")
-
-
-    # Объединение даты и времени
-    datetime_selected_2 = datetime.datetime.combine(date_2, time_2)
-
+# Call the user_u function to display the input fields
+user_u()
